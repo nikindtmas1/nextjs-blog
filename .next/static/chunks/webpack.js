3b709decd239ce9689243da86b9ac0bcac921ae2
Nikolai Nikolaev <niki_nik_74@abv.bg>
2023-03-22 13:12:37 +0200

    Update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e877e650e7adae5"; }
+/******/ 		__webpack_require__.h = function() { return "ae0868a0c250b350"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
